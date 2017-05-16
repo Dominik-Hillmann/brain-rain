5fb9da97cb916ff8a0654ca0461b9f43e34577ef
Dominik-Hillmann <dominik.hillmann@gmx.de>
2017-05-16 22:44:15 +0200

    Delete line.js

--- line.js
+++
@@ -1,6 +0,0 @@
-// These three lines adjust the middle line in the file to the summed height of the excerpts
-// creates a line because the background of the element is white
-
-var timeline = document.getElementById("timeline");
-var line = document.getElementById("line");
-line.style.height = (timeline.offsetHeight + 20) + "px";
