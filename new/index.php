41f7e3f5b7b1e9de67537b1ebfcd201c79fb7f2e
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-01-27 23:53:55 +0100

    Add files via upload

---
+++ new/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+
+<!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <title>BRAINRAIN</title>
+    <meta name="description" content=""><!--TODO BESCHREIBUNG NICHT VERGESSEN!-->
+    <meta name="author" content="Dominik Hillmann">
+
+<!-- Mobile Specific Metas -->
+    <meta>
+
+<!-- CSS links -->
+    <link rel="stylesheet" href="css/main.css">
+
+<!-- Favicon -->
+    <link rel="icon" type="image/png" href="pictures/favicon.png">
+
+</head>
+<body>
+   <header>
+      <div> <!-- Logo -->
+         <img src="pictures/logo.png"><h1>BRAINRAIN</h1>
+      </div>
+
+      <div id="topics"> <!--Ueberschriften -->
+         <div id="pictures"><img src="pictures/white_dot.png">PICTURES</div>
+         <div id="stories"><img src="pictures/white_dot.png">STORIES</div>
+         <div id="sketches"><img src="pictures/white_dot.png">SKETCHES</div>
+         <div id="contact"><img src="pictures/white_dot.png">CONTACT</div>
+      </div>
+   </header>
+
+<!-- Page Layout -->
+
+
+
+
+
+<!-- End of Page Layout- -->
+</body>
+</html>
+
+<!-- Links to Scripts -->
